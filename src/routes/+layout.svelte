1d367f4c1cacda1f5df0570500f967e7f601e377
Kuro <codewithkuro@gmail.com>
2025-04-12 23:41:28 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="navbar bg-base-100 shadow-sm">
+    <div class="flex-1">
+      <a href="/" class="btn btn-ghost text-xl">Wacky Picasso</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a href="/">My Photos</a></li>
+		<li><a href="/">Gallery</a></li>
+        <!-- <li>
+          <details>
+            <summary>Parent</summary>
+            <ul class="bg-base-100 rounded-t-none p-2">
+              <li><a href="/">Link 1</a></li>
+              <li><a href="/">Link 2</a></li>
+            </ul>
+          </details>
+        </li> -->
+      </ul>
+    </div>
+  </div>
+
+{@render children()}
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,24 @@
+<div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-start">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold text-left">upload a photo</h1>
+        <p class="py-6 text-left">
+          Let a drunk incompetent contemporary (AI) artist edit your photo
+        </p>
+
+        <fieldset class="fieldset">
+          <legend class="fieldset-legend">Pick a file</legend>
+          <input type="file" class="file-input" id="filein" />
+          <label class="fieldset-label" for="filein">Max size 2MB</label>
+        </fieldset>
+
+        <fieldset class="fieldset">
+          <legend class="fieldset-legend">How should I edit your photo?</legend>
+          <input type="text" class="input" placeholder="Make this photo into a cartoon" />          
+        </fieldset>
+
+        <button class="btn btn-primary my-4">Ruin my photo!</button>
+      </div>
+      
+    </div>
+  </div>
